5158dc9f80d3c7c1362b337724251694faaf74fe
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-25 22:58:02 +0530

    test

--- test/Token.js
+++ test/Token.js
@@ -1,35 +1,18 @@
 const {expect} = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Token Contract", function () {
-   
-    it("Deployment should assign the total supply of token to the owner", async function() {
 
-        const [owner] = await ethers.getSigners();
+    let Token;
+    let hardHatToken;
+    let owner;
+    let addr1;
+    let addr2;
+    let addr3;
 
-        const Token = await ethers.getContractFactory("Token"); // instace contract
-
-        const hardhatToken = await Token.deploy(); // deploy contract
-
-        const ownerBalance = await hardhatToken.getBalance(owner.address); // owner balance = 10000
-
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);  // total supply == 10000 
-    });
-    
-    it("Should Transfer Funds between accounts", async function() {
-
-        const [owner, addr1, addr2] = await ethers.getSigners();
-
-        const Token = await ethers.getContractFactory("Token"); // instace contract
-
-        const hardhatToken = await Token.deploy(); // deploy contract
-
-        // Transfer 10 tokens from owner to address1
-        await hardhatToken.transfer(addr1.address, 10);
-        expect(await hardhatToken.getBalance(addr1.address)).to.equal(10);
-        
-        // 5 token from addr1 tot addr2
-        await hardhatToken.connect(addr1).transfer(addr2.address, 5);
-        expect(await hardhatToken.getBalance(addr2.address)).to.equal(5);
-    });
-    
-});
+    beforeEach(async function() {
+        Token = await ethers.getContractFactory("Token");
+        [owner, addr1, addr2,...addrs] = await ethers.getSigners();
+        hardHatToken = await Token.deploy();
+    })
+})
