7c42477c5f8442375531cb2e38c0c2f497fdf30d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-25 11:55:38 +0530

    smartcontract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -22,7 +22,7 @@ contract Token {
         balances[to] += amount;
     }
 
-    function getBalance(address account) external view returns(uint) {
+    function getBalance(address account) external view returns(uint256) {
         return balances[account];
     }
 
